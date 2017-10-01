f9a0425b119ac8870eb32715def2e50a7d4adb64
rahul4times <ams@rahuls-air.lan>
2017-09-30 19:17:27 -0700

    Animation added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+  var mySkills = $('.skills');
+  $(mySkills).hide();
+
+  $(window).scroll(function(){
+    $(mySkills).fadeIn(2000);
+  });
+
+
+  // Typing animation
+  function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function typeWrite(span){
+  $('#'+span).addClass('cursor')
+  var text = $('#'+span).text();
+  var randInt = 0
+  for (var i = 0; i < text.length; i++) {
+    randInt += parseInt(randomIntFromInterval(40,300));
+    var typing = setTimeout(function(y){
+      $('#'+span).append(text.charAt(y));
+    },randInt, i);
+  };
+}
+
+typeWrite('typing');
+
+
+
+
+
+
+
 });
